0240c5fe394b7a7014bde0fff24f965d6f56acba
Alex Malagor <seo.dnja@gmail.com>
2021-03-21 23:14:06 +0300

    refactor: eslint-prettier

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import classes from './App.module.scss';
 import { APP_NAME } from 'appConstants';
+import classes from './App.module.scss';
 
 export function App() {
-  return (
-    <div className={classes.app}>
-      {APP_NAME}
-    </div>
-  );
+  return <div className={classes.app}>{APP_NAME}</div>;
 }
-
