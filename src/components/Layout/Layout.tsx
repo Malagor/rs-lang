3a8458696b690d9f3349ff120eb3fa5bd6f25d37
Alex Malagor <seo.dnja@gmail.com>
2021-03-22 15:05:14 +0300

    refactor: add database query

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 import { FooterWrapper, LayoutStyled, MainWrapper } from './styled';
 
 type LayoutProps = {};
 
 export const Layout: FC<LayoutProps> = ({ children }) => (
   <LayoutStyled>
-    <h1>Layout</h1>
+    <h1>RS-Lang. Team 53</h1>
+    <NavLink to="/">Main page</NavLink>
+    <NavLink to="/textbook">TextBook page</NavLink>
     <MainWrapper>{children}</MainWrapper>
     <FooterWrapper>footer</FooterWrapper>
   </LayoutStyled>
