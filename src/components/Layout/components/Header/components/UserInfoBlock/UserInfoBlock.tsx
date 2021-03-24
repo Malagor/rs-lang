739a7a142fbc5e0017782e57c64b4676c7850653
Alex Malagor <seo.dnja@gmail.com>
2021-03-24 21:22:54 +0300

    refactor: add emeil user to header

--- src/components/Layout/components/Header/components/UserInfoBlock/UserInfoBlock.tsx
+++ src/components/Layout/components/Header/components/UserInfoBlock/UserInfoBlock.tsx
@@ -3,12 +3,21 @@ import { Avatar, Typography } from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'modules/Login/selectors';
+import clsx from 'clsx';
 
 type UserInfoBlockProps = {};
 
 export const useStyles = makeStyles(() => ({
   marginLeft: {
     marginLeft: 10,
+  },
+  userInfoBlock: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  userEmail: {
+    fontSize: '14px',
+    lineHeight: '16px',
   },
 }));
 
@@ -18,7 +27,17 @@ export const UserInfoBlock: FC<UserInfoBlockProps> = () => {
 
   return (
     <>
-      <Typography className={classes.marginLeft}>{user.name}</Typography>
+      <div className={classes.userInfoBlock}>
+        <Typography className={classes.marginLeft} align="right">
+          {user.name}
+        </Typography>
+        <Typography
+          className={clsx(classes.marginLeft, classes.userEmail)}
+          align="right"
+        >
+          {user.email}
+        </Typography>
+      </div>
       <Avatar
         alt={user.name}
         src={user.avatar}
