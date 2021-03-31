6abc807fa97bb785275e69555a5b3a9005f171e5
Andrei <denisenko.andrei.da@gmail.com>
2021-03-31 19:31:20 +0300

    ref: deleted unused code

--- src/modules/GamesPage/GamesPage.tsx
+++ src/modules/GamesPage/GamesPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect } from 'react';
-import { Container, Paper, Grid } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { setPageTitle } from 'store/commonState/actions';
 import { gamesData } from 'appConstants/games';
