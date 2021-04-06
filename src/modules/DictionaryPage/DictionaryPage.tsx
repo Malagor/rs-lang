74132316eae6ea744175886d72fb468dc2f86c4b
Alex Malagor <seo.dnja@gmail.com>
2021-04-06 10:48:49 +0300

    refactor: add RedirectionModal on Dictionary page.

--- src/modules/DictionaryPage/DictionaryPage.tsx
+++ src/modules/DictionaryPage/DictionaryPage.tsx
@@ -1,17 +1,21 @@
 import React, { FC, useEffect } from 'react';
 import { setPageTitle } from 'store/commonState/actions';
 import { Container, Paper } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-import { NavGame } from 'components';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavGame, RedirectionModal } from 'components';
+import { selectUserId } from 'modules/Login/selectors';
 
 type DictionaryProps = {};
 
 export const DictionaryPage: FC<DictionaryProps> = () => {
   const dispatch = useDispatch();
+  const userId = useSelector(selectUserId);
 
   useEffect(() => {
     dispatch(setPageTitle('DictionaryPage'));
   }, [dispatch]);
+
+  if (!userId) return <RedirectionModal />;
 
   return (
     <Container>
