52b38e312d4ec7ad61bdb8f7bedc0d75cdd471a1
Alex Malagor <seo.dnja@gmail.com>
2021-04-07 17:18:51 +0300

    fix: add the check loading userId for Dictionary

--- src/modules/DictionaryPage/DictionaryPage.tsx
+++ src/modules/DictionaryPage/DictionaryPage.tsx
@@ -2,20 +2,22 @@ import React, { FC, useEffect } from 'react';
 import { setPageTitle } from 'store/commonState/actions';
 import { Container, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavGame, RedirectionModal } from 'components';
-import { selectUserId } from 'modules/Login/selectors';
+import { Loader, NavGame, RedirectionModal } from 'components';
+import { selectAuthLoadingStatus, selectUserId } from 'modules/Login/selectors';
 
 type DictionaryProps = {};
 
 export const DictionaryPage: FC<DictionaryProps> = () => {
   const dispatch = useDispatch();
   const userId = useSelector(selectUserId);
+  const isUserLoaded = useSelector(selectAuthLoadingStatus);
 
   useEffect(() => {
     dispatch(setPageTitle('DictionaryPage'));
   }, [dispatch]);
 
-  if (!userId) return <RedirectionModal />;
+  if (!userId && !isUserLoaded) return <RedirectionModal />;
+  if (!userId && isUserLoaded) return <Loader />;
 
   return (
     <Container>
