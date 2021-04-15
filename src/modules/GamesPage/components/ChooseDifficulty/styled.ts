156f2ed266adedf2f2992806ecc9a85ea133b23f
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-04-15 11:41:10 +0300

    add game name and return button to choose difficulty screen

--- src/modules/GamesPage/components/ChooseDifficulty/styled.ts
+++ src/modules/GamesPage/components/ChooseDifficulty/styled.ts
@@ -1,9 +1,13 @@
 import { Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { lighten, makeStyles } from '@material-ui/core/styles';
 
-import { COLOR_LAYOUT_DARK_GRAY } from 'appConstants/colors';
+import {
+  COLOR_LAYOUT_BACKGROUND,
+  COLOR_LAYOUT_DARK_GRAY,
+  COLOR_LAYOUT_TEXT,
+} from 'appConstants/colors';
 
-export const useStyles = makeStyles<Theme>((theme) => ({
+export const useStyles = makeStyles((theme: Theme) => ({
   root: {
     height: '100%',
     display: 'flex',
@@ -17,6 +21,30 @@ export const useStyles = makeStyles<Theme>((theme) => ({
     alignItems: 'center',
     width: '428px',
     paddingBottom: '100px',
+  },
+  gameNameContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '32px',
+  },
+  backButton: ({ gameColor }: { gameColor: string }) => ({
+    marginRight: '16px',
+    backgroundColor: gameColor,
+    boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.15)',
+    '&:hover': {
+      backgroundColor: lighten(gameColor, 0.2),
+    },
+  }),
+  backArrow: {
+    color: COLOR_LAYOUT_BACKGROUND,
+  },
+  gameName: {
+    margin: 0,
+    fontSize: '40px',
+    lineHeight: '40px',
+    fontWeight: 700,
+    color: COLOR_LAYOUT_TEXT,
   },
   selectHeader: {
     margin: 0,
